671d062de18ec57b760bd4f92ce54ac7a9529353
Ryan Kinney <ryan.kinney@maine.edu>
2022-04-12 16:13:27 -0400

    Add header files to camera_calibration.cpp

--- catkin_ws/src/calibration_project/src/camera_calibration.cpp
+++ catkin_ws/src/calibration_project/src/camera_calibration.cpp
@@ -1,3 +1,13 @@
-int main() {
+#include <opencv2/calib3d.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <Eigen/Dense>
+#include <opencv2/core/eigen.hpp>
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
 	return 0;
 }
