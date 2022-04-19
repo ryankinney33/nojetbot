2a8281f617ed794284ac333c5381ebfb3b316212
Carter <carter.q.stevens@maine.edu>
2022-04-19 15:23:14 -0400

    p_matrix

---
+++ catkin_ws/src/calibration_project/src/p_matrix.cpp
@@ -0,0 +1,39 @@
+#include <Eigen/Dense>
+#include <Eigen/SVD>
+#include <vector>
+#include <iostream>
+#include <opencv2/core/eigen.hpp>
+
+
+//Small issues I need the corresponding 2d image points and 3d world points
+Eigen::MatrixXd find_p(const std::vector<Eigen::Vector4d>& X_i, const std::vector<Eigen::Vector3d>& u_i)
+{
+	/*
+	if(points.size() < 0){
+		std::cerr << "Need more points to calculate P matrix\n";
+		throw std::runtime_error("Need more points");
+	}
+	*/
+	Eigen::MatrixXd P;
+	Eigen::Matrix3d u; //Write the cross product as a matrix operation
+	u << 0, -u_i(2), u_i(1),
+	  u_i(2), 0, -u_i(0),
+	  -u_i(1), u_i(0), 0;
+	Eigen::MatrixXd px;
+	/* Write P* X_i in terms of the row vectors, since all three terms like P_1.tranpose()* X_i are scalaras hence they are symmetric. p_1^T*X_i = X_I^T * P_1  */
+	px << X_i.transpose() * P.rows(0),
+	   X_i.transpose() * P.rows(1),
+	   X_i.transpose() * P.rows(2);
+    /*Matrix Multiplication with unknowns as a single vector, and the knows as a matrix multiplication with the unknowns */
+	Eigen::MatrixXd knowns;
+	knowns << 0, -u_i(2)*X_i.transpose(), u_i(1) * X_i,
+	      u_i(2) * X_i.transpose(), 0, -u_i(0) * X_i.transpose(),
+	      -u_i(1) * X.transpose(), u_i(0) * X_i.transpose(), 0;
+	unsigned int rows = knowns.row();
+	knowns.row(rows).setZero();
+
+	auto 
+
+	
+
+}
