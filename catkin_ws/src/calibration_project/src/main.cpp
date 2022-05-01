d3be6d44a9565c711da6563b41c8448c727d037f
Ryan Kinney <ryan.kinney@maine.edu>
2022-05-01 10:56:40 -0400

    Change the ROSNODE to use command-line arguments

--- catkin_ws/src/calibration_project/src/main.cpp
+++ catkin_ws/src/calibration_project/src/main.cpp
@@ -4,6 +4,7 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Image.h"
 #include <iostream>
+#include <vector>
 #include <cstdlib>
 
 #include <Eigen/Dense>
@@ -18,62 +19,87 @@
 #include <opencv2/core/eigen.hpp>
 #include <opencv2/imgproc.hpp>
 
-#include <iostream>
-#include <Eigen/Dense>
+static cv::Size patternsize; // holds the dimensions of the chessboard
+static std::vector<Eigen::Vector4d> X_i; // holds the 3d points of the chessboard corners
 
-int main()
+void imageCallback(const sensor_msgs::Image::ConstPtr& msg)
 {
+	// Read the image into an OpenCV image
+	cv_bridge::CvImageConstPtr cv_image_ptr;
+	try {
+		cv_image_ptr = cv_bridge::toCvShare(
+		msg,
+		sensor_msgs::image_encodings::RGB8);
+	} catch (cv_bridge::Exception& e) {
+		ROS_FATAL("cv_bridge exception: %s", e.what());
+		return;
+	}
+	auto img = cv_image_ptr->image.clone();
+	// Show the image on the screen
+	cv::imshow("IMG", img);
+
+	// Look for chessboard points
+	std::vector<Eigen::Vector3d> u_i;
+	bool success = get_chessboard_points(img, patternsize, u_i);
+	if (success) {
+		// Show the updated image
+		cv::imshow("IMG", img);
+
+		// Points found, get the P matrix
+		Eigen::MatrixXd P = find_p(X_i, u_i);
+
+		// Finally, find and print the camera matrixes
+		find_k(P);
+
+		ros::shutdown();
+	}
+}
+
+// The main function to setup everything and start the ros listener
+int main(int argc, char *argv[])
+{
+	if (argc != 5) {
+		std::cerr << "Usage: " << argv[0] << " gridWidth gridHeight squareSize bezelWidth\n";
+		return 1;
+	}
+
+	// Parse the command-line arguments to get the sizes
+	// they are image_path, grid width, grid height, square size (mm),
+	// vert. bezel size (mm), horiz. bezel size (mm)
+	int width, height;
+	double square_size, bezel_width;
+
+	try {
+		// extract width and height
+		width = std::stoi(argv[2]);
+		height = std::stoi(argv[3]);
+
+		// physical dimensions
+		square_size = std::stod(argv[4])/1000.0;
+		bezel_width = std::stod(argv[5])/1000.0;
+	} catch(const std::invalid_argument& e) {
+		// A conversion failed
+		std::cerr << "Error: arguments except the image path must be numerical.\n";
+		return 1;
+	} catch(const std::out_of_range& e) {
+		// Out of range error
+		std::cerr << "Error: argument out of range\n";
+		return 1;
+	}
+
+	// Make sure the numbers were positive
+	assert(width > 0 && height > 0 && square_size > 0 && bezel_width > 0);
+
+	// Set up the necessary data
+	patternsize = cv::Size(width, height); // number of centers
+	get_3d_points(width, height, square_size, bezel_width, X_i); // 3d chessboard points
+
+	// Start the ROS node
+	ros::init(argc, argv, "find_checkerboard");
+	ros::NodeHandle n;
+
+	ros::Subscriber sub = n.subscribe("/cv_camera/image_raw", 1, &imageCallback);
+	ros::spin();
+
 	return 0;
-}
-
-void imageCallback(const sensor_msgs::Image::ConstPtr& msg);
-void image_sub(int argc, char *argv[]){
-
-     ros::init(argc,argv, "find_checkerboard");
-     ros::NodeHandle n;
-
-     ros::Subscriber sub = n.subscribe("/cv_camera/image_raw",1,&imageCallback);
-     ros::spin();
-}
-void imageCallback(const sensor_msgs::Image::ConstPtr& msg) {
-
-    cv_bridge::CvImageConstPtr cv_image_ptr;
-    try {
-        cv_image_ptr = cv_bridge::toCvShare(
-            msg,
-            sensor_msgs::image_encodings::RGB8);
-    } catch (cv_bridge::Exception& e) {
-        ROS_FATAL("cv_bridge exception: %s", e.what());
-        return;
-    }
-
-    std::vector<Eigen::Vector3d> u_i;
-    cv::Size patternsize(6,8);
-    auto img = cv_image_ptr->image.clone();
-    bool success = get_chessboard_points(img,patternsize,u_i);
-    if(success){
-	std::cout << "u_i (transpose)\n";
-	for (const auto &iter : u_i) {
-		std::cout << iter(0) << "\t" << iter(1) << "\t" << iter(2) << "\n";
-	}
-
-	// Get and print the points X_i
-	std::vector<Eigen::Vector4d> X_i;
-	get_3d_points(6, 8, 0.009, 0.025, X_i);
-
-	std::cout << std::endl;
-	// Print the points to stdout
-	std::cout << "X_i (transpose)\n";
-	for (const auto &iter : X_i) {
-		std::cout << iter(0) << "\t" << iter(1) << "\t" << iter(2) << "\t" << iter(3) << "\n";
-	}
-
-	Eigen::MatrixXd P = find_p(X_i, u_i);
-	std::cout << "\nP:\n" << P << "\n\n";
-
-	// finally, find and print the camera matrices
-	find_k(P);
-
-	ros::shutdown();
-    }
 }
